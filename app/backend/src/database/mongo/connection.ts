97e55aeb80c085c84af2f1edcb1fe935f0ce684e
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-03-16 08:16:59 -0300

    back-end set up

--- app/backend/src/database/mongo/connection.ts
+++ app/backend/src/database/mongo/connection.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 
 const connectToDatabase = async (
-  mongoDatabaseURI = process.env.MONGO_URI || '',
-) => mongoose.connect(mongoDatabaseURI);
+  mongoDatabaseURI = process.env.MONGO_URI,
+): Promise<typeof mongoose> => mongoose.connect(mongoDatabaseURI as string);
 
 export default connectToDatabase;
